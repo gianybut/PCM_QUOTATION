e749bf3c7c35d8029e140269b892ed3234e26064
NathCaragao <nathanielcaragao@gmail.com>
2025-02-13 22:25:22 +0800

    Made basic express port listening

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 
+// Setup variables
 const app = express();
+const SERVER_PORT = process.env.SERVER_PORT;
 
 app.listen(5000, () => {
-  console.log("Express server started at port: 5000");
+  console.log(`Express server started at port: ${SERVER_PORT}`);
 });
