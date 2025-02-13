b1ee3f5566538aae096605f15623f4c48abff95e
NathCaragao <nathanielcaragao@gmail.com>
2025-02-13 22:30:09 +0800

    Added mongoose package

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import express from "express";
 const app = express();
 const SERVER_PORT = process.env.SERVER_PORT || 6969;
 
+// Making the server live
 app.listen(SERVER_PORT, () => {
   console.log(`Express server started at port: ${SERVER_PORT}`);
 });
