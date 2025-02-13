bc2a34436696fe0af6f4b5b14ab00e4846de8074
NathCaragao <nathanielcaragao@gmail.com>
2025-02-13 22:28:12 +0800

    Update index.js
    + added fallback value for PORT
    * changed PORT value to listen at by app

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ import express from "express";
 
 // Setup variables
 const app = express();
-const SERVER_PORT = process.env.SERVER_PORT;
+const SERVER_PORT = process.env.SERVER_PORT || 6969;
 
-app.listen(5000, () => {
+app.listen(SERVER_PORT, () => {
   console.log(`Express server started at port: ${SERVER_PORT}`);
 });
