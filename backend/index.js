18b2ee04b5c2c92a7935eea8f2a32269fd3ee768
NathCaragao <nathanielcaragao@gmail.com>
2025-02-14 17:38:57 +0800

    Connection to MongoDB Atlas successful

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
 
 // Setup variables
 const app = express();
 const SERVER_PORT = process.env.SERVER_PORT || 6969;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // Making the server live
-app.listen(SERVER_PORT, () => {
-  console.log(`Express server started at port: ${SERVER_PORT}`);
-});
+await mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully.");
+    // Starting server
+    app.listen(SERVER_PORT, () => {
+      console.log(`Express server started at port: ${SERVER_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed.");
+  });
