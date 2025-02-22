edbc9a7d2484e287c521d5a5b58a4c78efa8823a
NathCaragao <nathanielcaragao@gmail.com>
2025-02-22 13:28:18 +0800

    Delete Product working successfully

--- frontend/src/components/DeleteProductModal.jsx
+++ frontend/src/components/DeleteProductModal.jsx
@@ -2,6 +2,25 @@ import React, { useId, useState } from "react";
 import axios from "axios";
 
 const DeleteProductModal = ({ onClose, products }) => {
+  const BACKEND_URL = "http://localhost:6942";
+
+  const deleteProduct = async (productId) => {
+    await axios
+      .delete(`${BACKEND_URL}/products/delete/${productId}`)
+      .then((result) => {
+        alert("REMOVED PRODUCT SUCCESSFULLY.");
+      })
+      .catch((error) => {
+        alert("ERROR IN DELETING PRODUCT");
+      });
+  };
+
+  const handleDelete = async (productId) => {
+    await deleteProduct(productId);
+    onClose();
+    window.location.reload();
+  };
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
       <div className="bg-white rounded-lg w-[75%] p-6 overflow-y-scroll max-h-[75%]">
@@ -10,7 +29,20 @@ const DeleteProductModal = ({ onClose, products }) => {
         <div className="">
           {products.map((product) => {
             console.log(product);
-            return <div key={product["_id"]}>{product["productName"]}</div>;
+            return (
+              <div
+                key={product["_id"]}
+                className="border py-2 px-4 flex justify-between"
+              >
+                <span>{product["productName"]}</span>
+                <button
+                  className="text-red-500"
+                  onClick={() => handleDelete(product["_id"])}
+                >
+                  Remove
+                </button>
+              </div>
+            );
           })}
         </div>
       </div>
@@ -18,7 +50,7 @@ const DeleteProductModal = ({ onClose, products }) => {
       <button
         type="button"
         onClick={(e) => onClose(false)}
-        className="flex-1 p-2 bg-gray-500 text-white rounded-md hover:bg-gray-600"
+        className=" mt-4 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600"
       >
         Cancel
       </button>
--- frontend/src/components/Signature.jsx
+++ frontend/src/components/Signature.jsx
@@ -48,7 +48,7 @@ const Signature = () => {
   };
 
   return (
-    <div className="p-4 border-none print:mt-24">
+    <div className="p-4 border-none print:mt-48">
       <div className="mb-4">
         <input
           type="file"
