10e085c0b55da38b3c7f56d4913788e05962ae7f
NathCaragao <nathanielcaragao@gmail.com>
2025-02-24 17:31:01 +0800

    Added .bat script

---
+++ Launch Quotation System.bat
@@ -0,0 +1,22 @@
+@echo off
+REM Change to your desired directory path
+cd /d "C:\Users\Caragao\Desktop\OJT\Quotation System (Feb 2025)\PCM_QUOTATION"
+
+REM Set PowerShell execution policy and run npm commands
+powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
+
+REM Run npm start
+echo Starting the application...
+start /B cmd /c "npm run start"
+
+REM Wait a bit longer for the server to start (15 seconds instead of 5)
+echo Waiting for server to start...
+timeout /t 15
+
+REM Try to open the browser
+echo Opening browser...
+start "" "http://localhost:5173"
+
+REM Pause to keep the window open
+echo If the browser didn't open, try visiting http://localhost:5173 manually
+pause
