79a04425beee4404cc5676b214b57f795df6271d
NathCaragao <nathanielcaragao@gmail.com>
2025-02-27 15:51:41 +0800

    Merged frontend to backend

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import ProductRoute from "./Routes/ProductRoutes.js";
 import SizesRoute from "./Routes/SizesRoute.js";
 import axios from "axios";
+import path from "path";
 
 import PRODUCTS from "./PRODUCTS.json" with { type: "json" };
 import SIZES from "./SIZES.json" with { type: "json" };
@@ -12,11 +13,14 @@ const app = express();
 const SERVER_PORT = process.env.SERVER_PORT || 6942;
 const MONGODB_URI = process.env.MONGODB_URI;
 
+const __dirname = path.dirname(path.resolve());
+
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use("/products", ProductRoute);
 app.use("/sizes", SizesRoute);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 // Making the server live
 await mongoose
   .connect(MONGODB_URI)
@@ -24,6 +28,7 @@ await mongoose
     console.log("MongoDB connected successfully.");
     // Starting server
     app.listen(SERVER_PORT, () => {
+      console.log(__dirname);
       console.log(`Express server started at port: ${SERVER_PORT}`);
     });
   })
@@ -41,3 +46,8 @@ app.get("/addAllProductsAndSizes", async (req, res) => {
   });
   return res.send("OK");
 });
+
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
